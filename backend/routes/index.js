8b05046c2d29d3154a68cb72dd53a18b46907cd3
Asaf Add <asafadd@gmail.com>
2023-03-12 16:08:44 +0200

    Added creative starbootstrap by David Miller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,5 +4,10 @@ const mainController = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', mainController.home);
+router.get('/about', mainController.about);
+router.get('/contact', mainController.contact);
+router.get('/login', mainController.login);
+router.get('/register', mainController.register);
+router.get('/forgot-password', mainController.forgotpassword);
 
 module.exports = router;
